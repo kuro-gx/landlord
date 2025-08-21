718b5e937ecdbabf7455312267aacd7e0275369b
kuro <kuro__@139.com>
2025-08-21 19:28:11 +0800

    创建项目

---
+++ LandlordServer/Network/Socket/NetServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+public class NetServer {
+    private Socket _socket;
+
+    public void StartServer(string ip, int port) {
+        // 创建Socket
+        _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        // 设置IP和端口
+        EndPoint endPoint = new IPEndPoint(IPAddress.Parse(ip), port);
+        // 绑定IP和端口
+        _socket.Bind(endPoint);
+        // 设置最大连接数
+        _socket.Listen(50);
+
+        // 监听客户端连接
+        Thread listenThread = new Thread(ListenConnectSocket) {
+            IsBackground = true
+        };
+        listenThread.Start();
+    }
+
+    /// <summary>
+    /// 开始监听客户端连接
+    /// </summary>
+    private void ListenConnectSocket() {
+        // 开始监听
+        _socket.BeginAccept(ClientConnectHandle, null);
+    }
+
+    /// <summary>
+    /// 处理户端连接
+    /// </summary>
+    private void ClientConnectHandle(IAsyncResult ar) {
+        try {
+            // 结束监听
+            Socket clientSocket = _socket.EndAccept(ar);
+            // 开始接收客户端数据
+            Session session = new Session();
+            session.ReceiveData(clientSocket);
+            // 处理完当前客户端连接后，继续处理下一个客户端的连接请求
+            ListenConnectSocket();
+        } catch (Exception e) {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
---
+++ LandlordServer/Network/Socket/Session.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Sockets;
+
+public class Session {
+    private Socket _socket;
+    private byte[] buffer = new byte[1024 * 4]; // 接收数据的缓冲区
+
+    /// <summary>
+    /// 接收客户端数据
+    /// </summary>
+    /// <param name="socket">客户端连接对象</param>
+    public void ReceiveData(Socket socket) {
+        _socket = socket;
+        _socket.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, OnReceiveHandle, null);
+    }
+
+    /// <summary>
+    /// 处理客户端发送的数据
+    /// </summary>
+    private void OnReceiveHandle(IAsyncResult ar) {
+        // 本次接收数据的总字节数
+        int len = _socket.EndReceive(ar);
+        if (len > 0) {
+            while (true) {
+                
+            }
+        }
+    }
+}
